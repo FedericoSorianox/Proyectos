a6c5f04648dac1761d0ae82e30affd2858811a75
Your Name <your.email@example.com>
2025-07-04 09:35:22 -0300

    migration

---
+++ Badgers-Admin/backend/api/migrations/0012_replace_dots_in_pago_id.py
@@ -0,0 +1,19 @@
+from django.db import migrations
+
+def replace_dots_in_pago_id(apps, schema_editor):
+    Pago = apps.get_model('api', 'Pago')
+    for pago in Pago.objects.all():
+        nuevo_id = pago.id.replace('.', '_')
+        if nuevo_id != pago.id:
+            # Si ya existe un pago con ese nuevo_id, no lo cambiamos para evitar colisiones
+            if not Pago.objects.filter(id=nuevo_id).exists():
+                pago.id = nuevo_id
+                pago.save()
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('api', '0011_fix_en_vacaciones'),
+    ]
+    operations = [
+        migrations.RunPython(replace_dots_in_pago_id),
+    ] 
